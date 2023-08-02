48bdebeebb6a41f63a2488389eebe82d5bf95de3
roberto3219 <llamparoberto5@gmail.com>
2023-08-02 19:46:29 -0300

    mercado libre hecho

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const path = require('path')
 const app= express()
 
 const publicPath=path.resolve(__dirname,"./public")
+const port= process.env.PORT || 3400; 
 
 app.use(express.static(publicPath))
 
-app.listen(3400, ()=> 
-console.log("servidor corriendo"));
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/',(req,res)=> {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
